9e1c34dbde14bf830deaec30c77d4fc43110f5e2
Garrett Cullen <Garth619@users.noreply.github.com>
2020-10-19 12:55:44 -0700

    push

--- wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-1.php
+++ wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-1.php
@@ -1,5 +1,32 @@
 <section id='section-one'>
 
+<div id='sec-one-content'>
 
+  <span id='sec-one-subtitle'>Why Choose Us?</span><!-- sec-one-subtitle -->
+
+  <span id='sec-one-title'>We Win...Big</span><!-- sec-one-title -->
+
+  <span id='sec-one-descrip'>More verdicts & settlements over $10,000,000 than any law firm in California history.</span><!-- sec-one-descrip -->
+
+  <a class='button free-consult-button' href='<?php bloginfo('bloginfo');?>/href'>Request Free Consultation</a><!-- button -->
+
+</div><!-- sec-one-content -->
+
+<div id='sec-one-cr-wrapper'>
+
+  
+
+</div><!-- sec-one-cr-wrapper -->
+
+<picture>
+
+<!-- <source media='(min-width: 1170px)' data-srcset='' type='image/webp'>
+
+  <source media='(min-width: 1380px)' data-srcset=''> -->
+
+  <img id='hero' src='<?php bloginfo('template_directory'); ?>/images/hero-image-la-mobile.jpg' alt='' />
+
+</picture>
+<!-- remove 'data-' and 'lazyload' if needs to load on page load -->
 
 </section><!-- section-one -->
