0212479868e0dfd573f43d4efe24342ab7ffe47a
Garrett Cullen <Garth619@users.noreply.github.com>
2020-10-21 12:00:12 -0700

    push

--- wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-2.php
+++ wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-2.php
@@ -1,37 +1,121 @@
-<section id='section_two'>
+<section id='section-two'>
 
-	<div id='sec_two_inner' class="preload_section">
+	<div id='sec-two-inner' class="">
 
-		<?php if ( have_rows( 'section_two_selling_points' ) ) : ?>
-
-			<div id='sec_two_slider' class="preload_slider">
+		<div id='sec-two-left'>
+		
+			<img id='psb-playbook' src='<?php bloginfo('template_directory');?>/images/psb-playbook.png' alt=''/>
 			
-			<?php while ( have_rows( 'section_two_selling_points' ) ) : the_row(); ?>
+			<span id='sec-two-left-title'>Webinar Wednesday:</span><!-- sec-two-left-title -->
+
+			<span id='sec-two-descrip'>At 1pm (PDT) join Attorney Pete Kaufman & Shareholder Virginia Buchanan this Wednesday…</span><!-- sec-two-descrip -->
+
+			<a class='button sec-two-button' href=''>Register Now</a><!-- class -->
+		
+		</div><!-- sec-two-left -->
+
+		<div id='sec-two-right'>
+		
+			<span id='sec-two-title'>Breaking News And Current Cases</span><!-- sec-two-title -->
+
+			<div id='sec-two-slider'>
 			
-				<div class='sec_two_slide'>
-
-					<?php $icon = get_sub_field( 'icon' ); ?>
+				<div class='sec-two-slide'>
+				
+					<div class='sec-two-image-wrapper'>
 					
-					<?php if ( $icon ) { ?>
-						
-						<img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>" />
+						<img class='sec-two-image' src='<?php bloginfo('template_directory');?>/images/featurednews-1.jpg' alt=''/>
 					
-					<?php } ?>
+					</div><!-- sec-two-image-wrapper -->
 
-					<span class="amount"><?php the_sub_field( 'amount' ); ?></span>
+					<span class='sec-two-slide-cat'>Featured News</span><!-- sec-two-slide-cat -->
 
-					<span class='double_line'></span><!-- double_line -->
+					<span class='sec-two-slide-title'>$800 Million Settlement Reached with MGM for Route 91 Las Vegas Shooting Victims</span><!-- sec-two-slide-title -->
 
-					<span class='type'><?php the_sub_field( 'description' ); ?></span><!-- type -->
+					<span class='sec-two-slide-date'>October 3, 2019</span><!-- sec-two-slide-date -->
+				
+				</div><!-- sec-two-slide -->
 
-				</div><!-- sec_two_slide -->
+				<div class='sec-two-slide'>
+				
+					<div class='sec-two-image-wrapper'>
+					
+						<img class='sec-two-image' src='<?php bloginfo('template_directory');?>/images/featurednews-1.jpg' alt=''/>
+					
+					</div><!-- sec-two-image-wrapper -->
 
-			<?php endwhile; ?>
+					<span class='sec-two-slide-cat'>Featured News</span><!-- sec-two-slide-cat -->
 
-			</div><!-- sec_two_slider -->
+					<span class='sec-two-slide-title'>$800 Million Settlement Reached with MGM for Route 91 Las Vegas Shooting Victims</span><!-- sec-two-slide-title -->
 
-		<?php endif; ?>
+					<span class='sec-two-slide-date'>October 3, 2019</span><!-- sec-two-slide-date -->
+				
+				</div><!-- sec-two-slide -->
+
+				<div class='sec-two-slide'>
+				
+					<div class='sec-two-image-wrapper'>
+					
+						<img class='sec-two-image' src='<?php bloginfo('template_directory');?>/images/featurednews-1.jpg' alt=''/>
+					
+					</div><!-- sec-two-image-wrapper -->
+
+					<span class='sec-two-slide-cat'>Featured News</span><!-- sec-two-slide-cat -->
+
+					<span class='sec-two-slide-title'>$800 Million Settlement Reached with MGM for Route 91 Las Vegas Shooting Victims</span><!-- sec-two-slide-title -->
+
+					<span class='sec-two-slide-date'>October 3, 2019</span><!-- sec-two-slide-date -->
+				
+				</div><!-- sec-two-slide -->
+
+				<div class='sec-two-slide'>
+				
+					<div class='sec-two-image-wrapper'>
+					
+						<img class='sec-two-image' src='<?php bloginfo('template_directory');?>/images/featurednews-1.jpg' alt=''/>
+					
+					</div><!-- sec-two-image-wrapper -->
+
+					<span class='sec-two-slide-cat'>Featured News</span><!-- sec-two-slide-cat -->
+
+					<span class='sec-two-slide-title'>$800 Million Settlement Reached with MGM for Route 91 Las Vegas Shooting Victims</span><!-- sec-two-slide-title -->
+
+					<span class='sec-two-slide-date'>October 3, 2019</span><!-- sec-two-slide-date -->
+				
+				</div><!-- sec-two-slide -->
+
+				<div class='sec-two-slide'>
+				
+					<div class='sec-two-image-wrapper'>
+					
+						<img class='sec-two-image' src='<?php bloginfo('template_directory');?>/images/featurednews-1.jpg' alt=''/>
+					
+					</div><!-- sec-two-image-wrapper -->
+
+					<span class='sec-two-slide-cat'>Featured News</span><!-- sec-two-slide-cat -->
+
+					<span class='sec-two-slide-title'>$800 Million Settlement Reached with MGM for Route 91 Las Vegas Shooting Victims</span><!-- sec-two-slide-title -->
+
+					<span class='sec-two-slide-date'>October 3, 2019</span><!-- sec-two-slide-date -->
+				
+				</div><!-- sec-two-slide -->
+			
+			</div><!-- sec-two-slider -->
+
+			<div class='sec-two-arrow sec-two-arrow-left'>
+			
+				<?php echo file_get_contents( get_template_directory() . '/images/arrow-left.svg' ); ?>
+			
+			</div><!-- sec-two-arrow sec-two-arrow-left -->
+
+			<div class='sec-two-arrow sec-two-arrow-right'>
+			
+				<?php echo file_get_contents( get_template_directory() . '/images/arrow-right.svg' ); ?>
+			
+			</div><!-- sec-two-arrow sec-two-arrow-right -->
+		
+		</div><!-- sec-two-right -->
 	
-	</div><!-- sec_two_inner -->
+	</div><!-- sec-two-inner -->
 
-</section><!-- section_two -->
+</section><!-- section-two -->
--- wp-content/themes/psblaw/page-templates/template-home.php
+++ wp-content/themes/psblaw/page-templates/template-home.php
@@ -5,7 +5,7 @@
 	get_header();
 
 	get_template_part( 'page-templates/includes/homepage-template-parts/section', '1' );
-	// get_template_part( 'page-templates/includes/homepage-template-parts/section', '2' );
+	get_template_part( 'page-templates/includes/homepage-template-parts/section', '2' );
 	// get_template_part( 'page-templates/includes/homepage-template-parts/section', '3' );
 	// get_template_part( 'page-templates/includes/homepage-template-parts/section', '4' );
 	// get_template_part( 'page-templates/includes/homepage-template-parts/section', '5' );
