ec933fa78b82d2cc55f6f781261c2e088e346e1f
Garrett Cullen <Garth619@users.noreply.github.com>
2020-10-21 19:55:17 -0700

    push

--- wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-3.php
+++ wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-3.php
@@ -1,32 +1,56 @@
 <section id='section_three'>
 
 	<div id='sec_three_inner'>
-	
-		<h1 id='sec_three_header'><?php the_field( 'section_three_title' ); ?></h1><!-- sec_three_header -->
 
-		<span class='double_line'></span><!-- double_line -->
+		<div id='sec-three-header-wrapper'>
+		
+			<span id='sec-three-subtitle'>STRENGTH BY YOUR SIDE<span>TM</span></span><!-- sec-three-subtitle -->
 
-		<div id='sec_three_intro' class="content">
+			<h1 id='sec-three-header'>Los Angeles Personal Injury Lawyers</h1><!-- sec-three-header -->
 		
-			<?php the_field( 'section_three_intro' ); ?>
-		
-		</div><!-- sec_three_intro -->
-		
-		<div id='sec_three_col_wrapper' class="content">
-		
-			<div class='sec_three_col'>
-		
-				<?php the_field( 'section_three_column_one' ); ?>
-		
-			</div><!-- sec_three_col -->
+		</div><!-- sec-three-header-wrapper -->
 
-			<div class='sec_three_col'>
+		<div id='sec-three-content' class="content">
 		
-				<?php the_field( 'section_three_column_two' ); ?>
+			<div class='sec-three-col'>
+			
+				<div id='sec-three-video-wrapper'>
+				
+					<div id='sec-three-video-thumbnail'>
+					
+						<img src='<?php bloginfo('template_directory');?>/images/psb-video-image.jpg' alt=''/>
+
+						<div id='sec-three-video-overlay'>
+						
+							<div class='play-button'></div><!-- play-button -->
+						
+						</div><!-- sec-three-video-overlay -->
+					
+					</div><!-- sec-three-video-thumbnail -->
+
+					<span id='sec-three-video-descrip'>Learn in 90 seconds how Panish Shea Boyle, LLP can help you with your case.</span><!-- sec-three-video-descrip -->
+				
+				</div><!-- sec-three-video-wrapper -->
+
+				<div id='sec-three-intro'>
+				
+					<p>Panish Shea & Boyle LLP is a plaintiff’s personal injury law firm located in Los Angeles, California. Our trial lawyers focus solely on injury claims and are dedicated to protecting the rights of seriously injured people throughout Southern California.</p>
+				
+				</div><!-- sec-three-intro -->
+
+				<p>We regularly take on large corporations and government agencies or entities whose negligence causes our clients’ injuries and hold them accountable. Our top rated Los Angeles personal injury lawyers have always been champions of consumer rights. The size, clout or financial strength of wrongdoers is never a deterrent to our pursuit for justice.</p>
+				
+				<p>Our accident attorneys have the experience, skill and resources necessary to thoroughly investigate any injury case and achieve the best possible outcome for our clients. We have helped injury victims in cases involving car accidents, truck accidents, brain injury, spinal cord injury, medical malpractice, product defects, wrongful death and more. If you or a loved one have suffered a serious injury in Los Angeles, CA, our personal injury attorneys can help secure financial compensation for medical treatment, lost wages, pain, suffering and other losses you may have incurred.</p>
+				
+			</div><!-- sec-three-col -->
+
+			<div class='sec-three-col'>
+			
+			<h2>Panish Shea & Boyle LLP, the strength by your side<span>TM</span></h2>
+			
+			</div><!-- sec-three-col -->
 		
-			</div><!-- sec_three_col -->
-		
-		</div><!-- sec_three_col_wrapper -->
+		</div><!-- sec-three-content -->
 
 	</div><!-- sec_three_inner -->
 
