132365861802702cfdb97ca3415b7dfbeab31e68
Garrett Cullen <Garth619@users.noreply.github.com>
2020-10-25 19:42:49 -0700

    push

--- wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-5.php
+++ wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-5.php
@@ -2,7 +2,63 @@
 
 	<div id='sec-five-inner'>
 	
+		<div class='sec-five-col'>
+
+			<span id='sec-five-subtitle'>HERe TO HELP – ONCE AND FOR ALL<span>™</span></span><!-- sec-five-subtitle -->
+
+			<span id='sec-five-title'>The Attorneys at Panish Shea & Boyle, LLP</span><!-- sec-five-title -->
+
+			<div class='sec-five-col-inner content'>
+
+				<div id='sec-five-intro'>
+			
+					<p>At Panish Shea & Boyle, LLP, our skilled Los Angeles personal injury attorneys are dedicated to the relentless pursuit of justice for our clients.</p>
+			
+			</div><!-- sec-five-intro -->
+
+			<p>We have been recognized for excellence by other attorneys, legal organizations and publications across the nation. Our partners have handled high profile cases and secured record setting verdicts for our clients.</p>
+
+			<h3>Brian Panish</h3>
+			
+			<p>Brian Panish currently holds the national record for the largest jury verdict in a personal injury case. Anderson v. General Motors led to a $4.9 billion verdict in favor of the plaintiff; more than 75 other settlements and verdicts led to over $10 million; and more than 250 verdicts and settlements to over $1 million. Since 2011, Brian Panish focuses on personal injury, wrongful death, mass tort cases, and product liability claims in California and has received dozens of awards and acknowledgments for his legal acumen and record-setting verdicts and settlements. 
+			
+			<h3>Adam Shea</h3> 
+			
+			<p>Adam Shea focuses on truck and bus accident claims involving multi-vehicle crashes and pedestrian accidents. In the recent Barber v. Mossy Ford case, Adam Shea helped secure a $22.8 million award for his client and has won several more multimillion-dollar cases. Adam believes in aggressive representation for those harmed by negligence and intentional torts, and he travels throughout the country to speak at legal education events and seminars. </p>
+			
+		</div><!-- sec-five-col-inner -->
 		
+		</div><!-- sec-five-col -->
+
+		<div class='sec-five-col'>
+
+			<div class='sec-five-col-inner content'>
+
+				<h3>Kevin Boyle</h3> 
+			
+				<p>Kevin Boyle specializes in cases involving high-profile plaintiffs for things like wrongful death, catastrophic injuries, and fraud. Kevin recently represented two U.S. soldiers who suffered severe injuries while on deployment in Iraq due to a defective helicopter. The manufacturer faced the largest settlement of its kind thanks to Kevin’s representation. </p[>
+			
+				<h3>Rahul Ravipudi</h3>
+			
+				<p>Rahul Ravipudi recently won the award for 2017 Trial Lawyer of the Year by the Consumer Attorneys Association of Los Angeles thanks to his representation in a traumatic brain injury lawsuit. Mr. Ravipudi helped his client secure a $160 million jury verdict in April 2017 after his client sustained a severe beating at the hands of several security personnel.</p>
+
+				<div class='sec-five-image-wrapper'>
+
+					<span class='sec-five-image-title'>What to learn more about our accident attorneys?</span><!-- sec-five-image-title -->
+				
+					<div class='sec-five-image'>
+					
+						<img src='<?php bloginfo('template_directory');?>/images/partners-pic.jpg' alt=''/>
+					
+					</div><!-- sec-five-image -->
+
+					<a class='button sec-five-img-button' href=''>Meet our Attorneys</a><!-- button sec-five-img-button -->
+				
+				</div><!-- sec-five-image-wrapper -->
+
+			</div><!-- sec-five-col-inner -->
+		
+		</div><!-- sec-five-col -->
 	
 	</div><!-- sec-five-inner -->
 
