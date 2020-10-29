2db399f1ef06f834261bdc1cc2988c42e17c95c4
Garrett Cullen <Garth619@users.noreply.github.com>
2020-10-29 12:30:13 -0700

    push

--- wp-content/themes/psblaw/footer.php
+++ wp-content/themes/psblaw/footer.php
@@ -1,120 +1,122 @@
 <footer id="consultaton">
 
-  <div id='footer-top'>
+  <div id='footer-inner'>
   
-    <span id='footer-form-title'>How can we help you?</span><!-- footer-form-title -->
-
-    <span id='footer-form-descrip'>Fill out the form below for a<br/> no-obligation review of your case</span><!-- footer-form-descrip -->
-
-    <?php gravity_form(3, false, false, false, '', true, 1345); ?>
-
-    <span id='required'><span>*</span>Required Fields</span><!-- required -->
-  
-  </div><!-- footer-top -->
-
-  <div id='footer-bottom'>
-  
-    <span id='footer-bottom-title'>Panish Shea & Boyle, LLP is here to help - once and for all™</span><!-- footer-bottom-title -->
-
-    <div id='footer-bottom-col-wrapper'>
+    <div id='footer-top'>
     
-      <div class='footer-bottom-col'>
+      <span id='footer-form-title'>How can we help you?</span><!-- footer-form-title -->
+
+      <span id='footer-form-descrip'>Fill out the form below for a<br/> no-obligation review of your case</span><!-- footer-form-descrip -->
+
+      <?php gravity_form(3, false, false, false, '', true, 1345); ?>
+
+      <span id='required'><span>*</span>Required Fields</span><!-- required -->
+    
+    </div><!-- footer-top -->
+
+    <div id='footer-bottom'>
+    
+      <span id='footer-bottom-title'>Panish Shea & Boyle, LLP is here to help - once and for all™</span><!-- footer-bottom-title -->
+
+      <div id='footer-bottom-col-wrapper'>
       
-        <span class='footer-bottom-col-title'>Los Angeles</span><!-- footer-bottom-title -->
-
-        <span class='footer-bottom-content'>11111 Santa Monica Blvd #700,<br/> Los Angeles, CA 90025</span><!-- footer-bottom-content -->
-
-        <a class='footer-bottom-content' href="tel:+13104771700">(310) 477-1700</a><!-- footer-bottom-content -->
-
-        <a class='footer-bottom-content get-directions-button' href="https://www.google.com/maps/place/11111+Santa+Monica+Blvd+%23700,+Los+Angeles,+CA+90025/@34.0484137,-118.4476138,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2bb77512ee2f7:0xe3c37c4d687a647a!8m2!3d34.0484137!4d-118.4454251" target="_blank" rel="noopener">Directions</a><!-- get-directions-button -->
-      
-      </div><!-- footer-bottom-col -->
-
-      <div class='footer-bottom-col'>
-      
-        <span class='footer-bottom-col-title'>Newport Beach</span><!-- footer-bottom-title -->
-
-        <span class='footer-bottom-content'>5160 Birch St #210,<br/> Newport Beach, CA 92660</span><!-- footer-bottom-content -->
-
-        <a class='footer-bottom-content' href="tel:+9494685777">(949) 468-5777</a><!-- footer-bottom-content -->
-
-        <a class='footer-bottom-content get-directions-button' href="https://www.google.com/maps/place/5160+Birch+St+%23210,+Newport+Beach,+CA+92660/@33.6646878,-117.8593905,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcde5dd5f3fa9d:0xff9dd9da0a4ce9ce!8m2!3d33.6646834!4d-117.8571965" target="_blank" rel="noopener">Directions</a><!-- get-directions-button -->
-      
-      </div><!-- footer-bottom-col -->
-
-      <div class='footer-bottom-col'>
-      
-        <div class='footer-bottom-sub-col'>
+        <div class='footer-bottom-col'>
         
-          <span class='footer-bottom-col-title'>Toll Free</span><!-- footer-bottom-title -->
+          <span class='footer-bottom-col-title'>Los Angeles</span><!-- footer-bottom-title -->
 
-          <a class='footer-bottom-content' href="tel:+8884986487">(888) 498-6487</a><!-- footer-bottom-content -->
-        
-        </div><!-- footer-bottom-sub-col -->
+          <span class='footer-bottom-content'>11111 Santa Monica Blvd #700,<br/> Los Angeles, CA 90025</span><!-- footer-bottom-content -->
 
-        <div class='footer-bottom-sub-col'>
-        
-          <span class='footer-bottom-col-title'>Fax</span><!-- footer-bottom-title -->
+          <a class='footer-bottom-content' href="tel:+13104771700">(310) 477-1700</a><!-- footer-bottom-content -->
 
-          <a class='footer-bottom-content'>(888) 498-6487</a><!-- footer-bottom-content -->
+          <a class='footer-bottom-content get-directions-button' href="https://www.google.com/maps/place/11111+Santa+Monica+Blvd+%23700,+Los+Angeles,+CA+90025/@34.0484137,-118.4476138,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2bb77512ee2f7:0xe3c37c4d687a647a!8m2!3d34.0484137!4d-118.4454251" target="_blank" rel="noopener">Directions</a><!-- get-directions-button -->
         
-        </div><!-- footer-bottom-sub-col -->
+        </div><!-- footer-bottom-col -->
 
-        <div class='footer-bottom-sub-col'>
+        <div class='footer-bottom-col'>
         
-          <div id='social-media-wrapper'>
+          <span class='footer-bottom-col-title'>Newport Beach</span><!-- footer-bottom-title -->
+
+          <span class='footer-bottom-content'>5160 Birch St #210,<br/> Newport Beach, CA 92660</span><!-- footer-bottom-content -->
+
+          <a class='footer-bottom-content' href="tel:+9494685777">(949) 468-5777</a><!-- footer-bottom-content -->
+
+          <a class='footer-bottom-content get-directions-button' href="https://www.google.com/maps/place/5160+Birch+St+%23210,+Newport+Beach,+CA+92660/@33.6646878,-117.8593905,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcde5dd5f3fa9d:0xff9dd9da0a4ce9ce!8m2!3d33.6646834!4d-117.8571965" target="_blank" rel="noopener">Directions</a><!-- get-directions-button -->
+        
+        </div><!-- footer-bottom-col -->
+
+        <div class='footer-bottom-col'>
+        
+          <div class='footer-bottom-sub-col'>
           
-            <a href="" target="_blank" rel="noopener">
+            <span class='footer-bottom-col-title'>Toll Free</span><!-- footer-bottom-title -->
 
-              <?php echo file_get_contents( get_template_directory() . '/images/social-ig.svg' ); ?>
+            <a class='footer-bottom-content' href="tel:+8884986487">(888) 498-6487</a><!-- footer-bottom-content -->
+          
+          </div><!-- footer-bottom-sub-col -->
+
+          <div class='footer-bottom-sub-col'>
+          
+            <span class='footer-bottom-col-title'>Fax</span><!-- footer-bottom-title -->
+
+            <a class='footer-bottom-content'>(888) 498-6487</a><!-- footer-bottom-content -->
+          
+          </div><!-- footer-bottom-sub-col -->
+
+          <div class='footer-bottom-sub-col'>
+          
+            <div id='social-media-wrapper'>
+            
+              <a href="" target="_blank" rel="noopener">
+
+                <?php echo file_get_contents( get_template_directory() . '/images/social-ig.svg' ); ?>
+              
+              </a>
+
+              <a href="" target="_blank" rel="noopener">
+
+              <?php echo file_get_contents( get_template_directory() . '/images/social-twitter.svg' ); ?>
             
             </a>
 
             <a href="" target="_blank" rel="noopener">
 
-            <?php echo file_get_contents( get_template_directory() . '/images/social-twitter.svg' ); ?>
+              <?php echo file_get_contents( get_template_directory() . '/images/social-fb.svg' ); ?>
+            
+            </a>
+            
+            </div><!-- social-media-wrapper -->
           
-          </a>
-
-          <a href="" target="_blank" rel="noopener">
-
-            <?php echo file_get_contents( get_template_directory() . '/images/social-fb.svg' ); ?>
-          
-          </a>
-          
-          </div><!-- social-media-wrapper -->
+          </div><!-- footer-bottom-sub-col -->
         
-        </div><!-- footer-bottom-sub-col -->
+        </div><!-- footer-bottom-col -->
       
-      </div><!-- footer-bottom-col -->
-    
-    </div><!-- footer-bottom-col-wrapper -->
-    
-  </div><!-- footer-bottom -->
+      </div><!-- footer-bottom-col-wrapper -->
+      
+    </div><!-- footer-bottom -->
 
-  <div id='copyright'>
-  
-    <div id='copyright-inner'>
+    <div id='copyright'>
     
-      <ul>
-        <li>Copyright &copy; <?php $pfx_date = get_the_date(); echo $pfx_date; ?> Panish Shea & Boyle</li>
-        <li>All rights reserved.</li>
-        <li><a href="">DISCLAIMER</a></li>
-        <li><a href="" target="_blank" rel="noopener">SITEMAP</a></li>
-        <li><a href="" target="_blank" rel="noopener">Google Maps</a></li>
-      </ul>
+      <div id='copyright-inner'>
+      
+        <ul>
+          <li>Copyright &copy; <?php $pfx_date = get_the_date(); echo $pfx_date; ?> Panish Shea & Boyle</li>
+          <li>All rights reserved.</li>
+          <li><a href="">DISCLAIMER</a></li>
+          <li><a href="" target="_blank" rel="noopener">SITEMAP</a></li>
+          <li><a href="" target="_blank" rel="noopener">Google Maps</a></li>
+        </ul>
 
-      <a id="ilawyer" href="//ilawyermarketing.com" target="_blank" rel="noopener">
-  
-        <img src='<?php bloginfo('template_directory');?>/images/ilawyerlogo.svg' alt=''/>
-  
-      </a>
+        <a id="ilawyer" href="//ilawyermarketing.com" target="_blank" rel="noopener">
     
-    </div><!-- copyright-inner -->
-  
-  </div><!-- copyright -->
+          <img src='<?php bloginfo('template_directory');?>/images/ilawyerlogo.svg' alt=''/>
+    
+        </a>
+      
+      </div><!-- copyright-inner -->
+    
+    </div><!-- copyright -->
 
-  
+  </div><!-- footer-inner -->
 
 </footer>
 
