a8e6ce2cce0f624acc6020454d2c4da30098ddc3
Garrett Cullen <Garth619@users.noreply.github.com>
2020-11-02 18:45:28 -0800

    push

--- wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-5.php
+++ wp-content/themes/psblaw/page-templates/includes/homepage-template-parts/section-5.php
@@ -18,11 +18,11 @@
 
 			<p>We have been recognized for excellence by other attorneys, legal organizations and publications across the nation. Our partners have handled high profile cases and secured record setting verdicts for our clients.</p>
 
-			<h3>Brian Panish</h3>
+			<h2>Brian Panish</h2>
 			
 			<p>Brian Panish currently holds the national record for the largest jury verdict in a personal injury case. Anderson v. General Motors led to a $4.9 billion verdict in favor of the plaintiff; more than 75 other settlements and verdicts led to over $10 million; and more than 250 verdicts and settlements to over $1 million. Since 2011, Brian Panish focuses on personal injury, wrongful death, mass tort cases, and product liability claims in California and has received dozens of awards and acknowledgments for his legal acumen and record-setting verdicts and settlements. 
 			
-			<h3>Adam Shea</h3> 
+			<h2>Adam Shea</h2> 
 			
 			<p>Adam Shea focuses on truck and bus accident claims involving multi-vehicle crashes and pedestrian accidents. In the recent Barber v. Mossy Ford case, Adam Shea helped secure a $22.8 million award for his client and has won several more multimillion-dollar cases. Adam believes in aggressive representation for those harmed by negligence and intentional torts, and he travels throughout the country to speak at legal education events and seminars. </p>
 			
@@ -36,11 +36,11 @@
 
 				<div class='sec-five-col-inner-two'>
 				
-					<h3>Kevin Boyle</h3> 
+					<h2>Kevin Boyle</h2> 
 			
-					<p>Kevin Boyle specializes in cases involving high-profile plaintiffs for things like wrongful death, catastrophic injuries, and fraud. Kevin recently represented two U.S. soldiers who suffered severe injuries while on deployment in Iraq due to a defective helicopter. The manufacturer faced the largest settlement of its kind thanks to Kevin’s representation. </p[>
+					<p>Kevin Boyle specializes in cases involving high-profile plaintiffs for things like wrongful death, catastrophic injuries, and fraud. Kevin recently represented two U.S. soldiers who suffered severe injuries while on deployment in Iraq due to a defective helicopter. The manufacturer faced the largest settlement of its kind thanks to Kevin’s representation. </p>
 		
-					<h3>Rahul Ravipudi</h3>
+					<h2>Rahul Ravipudi</h2>
 		
 					<p>Rahul Ravipudi recently won the award for 2017 Trial Lawyer of the Year by the Consumer Attorneys Association of Los Angeles thanks to his representation in a traumatic brain injury lawsuit. Mr. Ravipudi helped his client secure a $160 million jury verdict in April 2017 after his client sustained a severe beating at the hands of several security personnel.</p>
 				
--- wp-content/themes/psblaw/page.php
+++ wp-content/themes/psblaw/page.php
@@ -2,29 +2,60 @@
 
 <div id="internal-main">
 
-<div id='two-col-template-with-banner'>
-
 	<?php get_template_part('page-templates/includes/template','default-page-banner'); ?>
-
-</div><!-- two-col-template-with-banner -->
-
-<div class="page-container two-col">
+	
+	<div id="page-container" class="two-col">
 		
 		<div class="page-content">
 			
 			<?php if(get_field('banner_h1') == "Yes") : ?>
 			
-				<h2 class="page-header"><?php the_title();?></h2>
+				<h2 class="page-title page-content-title"><?php the_title();?></h2>
 			
 			<?php else:?>
 			
-				<h1 class="page-header"><?php the_title();?></h1>
+				<h1 class="page-title page-content-title"><?php the_title();?></h1>
 			
 			<?php endif;?>
 
 			<div class='page-content-inner content'>
+
+			<h2>H2 Style Panish Shea & Boyle LLP is a plaintiff’s personal injury law firm located in Los Angeles, California.</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta eget eros in venenatis. Maecenas non urna ante. Nullam tortor massa, tincidunt eu ullamcorper et, mollis sit amet turpis. Donec tristique nibh vel metus maximus, eu laoreet mauris mollis. Maecenas in commodo ex. Pellentesque protector.</p>
+
+<p>Suspendisse semper tincidunt libero eu placerat. Nullam libero quam, ultricies a tellus ultrices, aliquam lacinia ligula. Praesent sed nulla rhoncus, lacinia enim nec, hendrerit nunc. Donec volutpat vitae lorem at porttitor. Etiam sed sodales sapien. Donec rhoncus nisl vitae euismod viverra. Etiam mi massa, suscipit vel fringilla at, molestie id eros. Nullam sem sapien, aliquet non bibendum ut, dignissim vel odio. In sagittis purus elit, eu imperdiet dui rhoncus ut. Sed et suscipit mauris. Proin sodales tortor at risus pretium eleifend. Praesent ipsum dui, luctus id ultrices ut, tristique eget justo. Etiam sit amet ex urna. Integer porta tempus lorem, non fringilla massa consectetur vitae. In est sem, euismod ut nibh quis, lobortis venenatis odio. Suspendisse id massa at arcu volutpat pellentesque eget non libero. </p>
+
+<a href="">Link style</a> tincidunt leo mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis nisi. Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante. Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus.
+
+<div class="blockquote"><p>“The clients that we represent come to us because they’ve got some wrongs that need to be made right, and there’s no one else that’s going to do it but us.”</p>
+<span>Brian Panish</span>
+</div><!-- blockquote -->
+
+<h3>H3 STYLE STRENGTH BY YOUR SIDET<span>TM</span></h3>
+
+<p>Etiam a tincidunt leo. Maecenas mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis nisi. Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante. Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus. Curabitur tincidunt diam augue, a placerat erat auctor vitae. Sed laoreet leo convallis, posuere nunc quis, blandit mauris. Curabitur vehicula, est vel tempor venenatis, nibh sem fringilla nisl, sodales eleifend dui nibh id ipsum. Sed cursus nisl id lorem aliquet tempus. Nullam aliquam, sem vel faucibus finibus, est turpis vestibulum ligula, in laoreet nibh sem ut nisi. </p>
+
+<h4>H4 Panish Shea & Boyle LLP, the strength by your side<span>TM.</span></h4>
+<ul>
+<li>List style aecenas mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis.</li>
+<li>Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante.</li> 
+<li>Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus.</li>
+<li>Curabitur tincidunt diam augue, a placerat erat auctor vitae. Sed laoreet leo convallis, posuere nunc quis, blandit mauris. Curabitur vehicula, est vel tempor venenatis, nibh sem fringilla nisl, eleifend dui nibh id ipsum. Sed cursus nisl id lorem aliquet tempus. Nullam aliquam, sem vel faucibus finibus, est turpis vestibulum ligula, in laoreet nibh sem ut nisi.</li>
+</ul>
+
+<p>Etiam a tincidunt leo. Maecenas mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis nisi. Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante. Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus. Curabitur tincidunt diam augue, a placerat erat auctor vitae. Sed laoreet leo convallis, posuere nunc quis, blandit mauris. Curabitur vehicula, est vel tempor venenatis, nibh sem fringilla nisl, sodales eleifend dui nibh id ipsum. Sed cursus nisl id lorem aliquet tempus. Nullam aliquam, sem vel faucibus finibus, est turpis vestibulum ligula, in laoreet nibh sem ut nisi</p>
+
+<ol>
+<li>List style aecenas mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis.</li>
+<li>Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante.</li> 
+<li>Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus.</li>
+<li>Curabitur tincidunt diam augue, a placerat erat auctor vitae. Sed laoreet leo convallis, posuere nunc quis, blandit mauris. Curabitur vehicula, est vel tempor venenatis, nibh sem fringilla nisl, eleifend dui nibh id ipsum. Sed cursus nisl id lorem aliquet tempus. Nullam aliquam, sem vel faucibus finibus, est turpis vestibulum ligula, in laoreet nibh sem ut nisi.</li>
+</0l> 
+
+<p>Etiam a tincidunt leo. Maecenas mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis nisi. Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante. Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus. Curabitur tincidunt diam augue, a placerat erat auctor vitae. Sed laoreet leo convallis, posuere nunc quis, blandit mauris. Curabitur vehicula, est vel tempor venenatis, nibh sem fringilla nisl, sodales eleifend dui nibh id ipsum. Sed cursus nisl id lorem aliquet tempus. Nullam aliquam, sem vel faucibus finibus, est turpis vestibulum ligula, in laoreet nibh sem ut nisi. </p>
 			
-			<?php get_template_part( 'loop', 'page' ); ?>
+			<?php // get_template_part( 'loop', 'page' ); ?>
 			
 			</div><!-- page-content-inner -->
 			
