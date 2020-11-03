85167aab93264ce283a6f7d9090a7a682bed7864
Garrett Cullen <Garth619@users.noreply.github.com>
2020-11-03 15:33:51 -0800

    push

--- wp-content/themes/psblaw/page.php
+++ wp-content/themes/psblaw/page.php
@@ -29,7 +29,7 @@
 <a href="">Link style</a> tincidunt leo mattis tortor fermentum lacinia placerat. Morbi nibh massa, maximus nec eros et, varius lobortis nisi. Ut a nisl nec elit vestibulum varius et quis nunc. Praesent in dapibus ante. Nullam non diam eget ex fringilla varius eu ac risus. Morbi eleifend varius facilisis. Sed vitae augue lacus.
 
 <div class="blockquote"><p>“The clients that we represent come to us because they’ve got some wrongs that need to be made right, and there’s no one else that’s going to do it but us.”</p>
-<span>Brian Panish</span>
+<span>-Brian Panish</span>
 </div><!-- blockquote -->
 
 <h3>H3 STYLE STRENGTH BY YOUR SIDE<span>TM</span></h3>
