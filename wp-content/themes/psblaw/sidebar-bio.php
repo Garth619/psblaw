4a2f9a64d5fe6e01561da1259b6b8b16c1f75f1d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-11-06 16:32:18 -0800

    push

--- wp-content/themes/psblaw/sidebar-bio.php
+++ wp-content/themes/psblaw/sidebar-bio.php
@@ -18,17 +18,45 @@
 
 </div><!-- widget -->
 
-<div class='widget'>
+<div class='widget articles'>
 
 	<h3>Articles by Brian Panish</h3>
 
-	<ul>
-		<li><a>test</a></li>
-		<li><a>test</a></li>
-		<li><a>test</a></li>
-		<li><a>test</a></li>
-		<li><a>test</a></li>
-	<ul>
+	<div id='bio-slider-wrapper'>
+
+		<div id='bio-slider'>
+		
+			<div class='bio-slide'>
+			
+				<span class='bio-slide-title'>Experimenting with the Consumer</span><!-- bio-slide-title -->
+
+				<div class='bio-slide-content content'>
+				
+					<p><a href="">Trial Magazine</a> – Brian Panish gives his review of Marshall S. Shapo’s book, “Experimenting with the Consumer.”</p>
+				
+				</div><!-- bio-slide-content content -->
+			
+			</div><!-- bio-slide -->
+		
+		</div><!-- bio-slider -->
+
+		<div class='bio-slider-arrow-wrapper'>
+		
+			<div class='bio-arrow bio-arrow-left'>
+			
+				<?php echo file_get_contents( get_template_directory() . '/images/arrow-left.svg' ); ?>
+			
+			</div><!-- bio-arrow bio-arrow-left -->
+
+			<div class='bio-arrow bio-arrow-right'>
+			
+				<?php echo file_get_contents( get_template_directory() . '/images/arrow-right.svg' ); ?>
+			
+			</div><!-- bio-arrow bio-arrow-right -->
+		
+		</div><!-- bio-slider-arrow -->
+
+	</div><!-- bio-slider-wrapper -->
 
 </div><!-- widget -->
 			
