f9743aece4dbcfbe46a5a486be825f441c6eeba2
Garrett Cullen <Garth619@users.noreply.github.com>
2020-11-08 19:38:45 -0800

    push

--- wp-content/themes/psblaw/page-templates/template-about.php
+++ wp-content/themes/psblaw/page-templates/template-about.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <div id="internal-main">
 
-<h1 class="page-title page-large-content-title page-about-title"><?php the_title();?></h1>
+<h1 class="page-title page-large-content-title page-about-title"><?php the_field( 'top_title' ); ?></h1>
 	
 <div id='about-top'>
 
@@ -15,11 +15,11 @@ get_header(); ?>
 
 			<div id='page-descrip-wrapper'>
 
-			<span id='page-subtitle'>PANISh SHEA & BOYLE, LLP IS HERE TO HELP – ONCE AND FOR ALL™</span><!-- about-top-subtitle -->
+			<span id='page-subtitle'><?php the_field( 'top_subtitle' ); ?></span><!-- about-top-subtitle -->
 
 				<div id='page-descrip'>
 
-					<p>When a tragedy happens, you want an experienced trial attorney to navigate you through the complex world of personal injury, wrongful death litigation and business litigation. Choosing the right firm is critical in recovering all of the damages you are entitled to receive.  The attorneys at Panish Shea & Boyle LLP are here to be the Strength By Your Side™.</p>
+					<?php the_field( 'top_description' ); ?>
 	
 					<?php //the_field( 'practice_area_description' ); ?>
 	
@@ -35,13 +35,7 @@ get_header(); ?>
 
 	<div class='about-col'>
 
-		<p>Panish Shea & Boyle LLP has extensive experience with the often complicated lawsuits that arise after an injury or death caused by another’s negligence or misconduct. Our attorneys have been repeatedly recognized for excellence by other trial attorneys, legal organizations and publications nationwide. The firm has been ranked by U.S. News & World Report and Best Lawyers® as a “Tier 1” Firm in the areas of Plaintiffs Personal Injury Litigation, Plaintiffs Product Liability Litigation and Plaintiffs Mass Tort Litigation/Class Actions – the highest ranking a firm can receive – and is recognized as being among the top Plaintiff’s law firms in the country by the National Law Journal. </p>
-
-		<p>Once we accept a case, we commit to you and your cause and take the steps necessary to get you the compensation you deserve. Because we limit the number of cases we accept, our attorneys and staff are able to focus their efforts on you and your case and put in the time and resources it takes to achieve the best possible result. We understand that dealing with a life-altering injury, the death or injury of a family member or other challenges caused by the wrongful act of another is difficult emotionally and financially. Your case will be handled with compassion and integrity, and we provide you with personal attention and support throughout the process.</p> 
-
-		<h3>We handle cases nationwide, as well as statewide in both California & Nevada.</h3>
-
-		<p>Our Southern California personal injury attorneys have their home base in our main office in Los Angeles, CA. We also serve the communities of Orange County, San Bernardino, and Riverside, California. We handle Nevada cases from our office in Las Vegas, Nevada.</p>
+		<?php the_field( 'top_content' ); ?>
 
 	</div><!-- about-col -->
 
@@ -51,17 +45,19 @@ get_header(); ?>
 		
 			<div id='about-img'>
 			
-				<img src='<?php bloginfo('template_directory');?>/images/about-image.jpg' alt=''/>
+				<?php $top_image = get_field( 'top_image' ); ?>
+				
+				<?php if ( $top_image ) { ?>
+				
+					<img src="<?php echo $top_image['url']; ?>" alt="<?php echo $top_image['alt']; ?>" />
+				
+				<?php } ?>
 			
 			</div><!-- about-img -->
 		
 		</div><!-- about-img-wrapper -->
 
-		<h4>The History of Our Firm</h4>
-
-		<p>In 2005, three lawyers with extensive track records of litigation against negligent corporate giants formed Panish Shea & Boyle LLP. </p>
-
-		<p>Partners Brian Panish, Adam Shea and Kevin Boyle vowed to fight corporate greed and protect consumers from corporations that strive for profits over the safety of their customers. The three founding partners worked together previously and found their shared advocacy for the consumer a driving force behind founding a new firm dedicated to working for you. Since then, Panish Shea & Boyle LLP has added Partner Rahul Ravipudi, an attorney with a similar focus on providing consumers with class action recourse against large corporations engaging in unfair practices.</p>
+		<?php the_field( 'top_content_two' ); ?>
 
 	</div><!-- about-col -->
 
@@ -73,231 +69,109 @@ get_header(); ?>
 
 		<div id='about-middle'>
 
-			<span id='about-middle-title'>Our Partners</span><!-- about-middle-title -->
+			<span id='about-middle-title'><?php the_field( 'middle_title' ); ?></span><!-- about-middle-title -->
 
 			<div id='about-middle-col-wrapper'>
 			
 				<div class='about-middle-col'>
+
+					<?php if ( have_rows( 'about_attorneys' ) ) : ?>
+					
+						<?php while ( have_rows( 'about_attorneys' ) ) : the_row(); ?>
+						
+							<?php $post_object = get_sub_field( 'about_attorney' ); ?>
+							
+							<?php if ( $post_object ): ?>
+							
+								<?php $post = $post_object; ?>
+								<?php setup_postdata( $post ); ?> 
+
+								<div class='about-middle-att'>
+						
+									<?php $attorney_profile = get_field( 'attorney_profile' ); ?>
+									
+									<?php if ( $attorney_profile ) { ?>
+									
+										<img src="<?php echo $attorney_profile['url']; ?>" alt="<?php echo $attorney_profile['alt']; ?>" />
+									
+									<?php } ?>
+
+									<span class='about-middle-att-title'><?php the_title();?></span><!-- about-middle-att-title -->
+
+									<div class='about-middle-att-content content'>
+						
+										<?php the_sub_field( 'excerpt' ); ?>
+
+									<div class='additional-content'>
+							
+									<?php the_sub_field( 'content' ); ?>
+							
+								</div><!-- additional-content -->
+						
+							</div><!-- about-middle-att-content -->
+
+							<a class='button-three about-read-more'>Read More</a><!-- button-two -->
+					
+						</div><!-- about-middle-att -->
+								
+					<?php wp_reset_postdata(); ?>
+							
+				<?php endif; ?>
+			
+			<?php endwhile; ?>
+
+		<?php endif; ?>
 				
-					<div class='about-middle-att'>
-					
-						<img src='<?php bloginfo('template_directory');?>/images/att-brian-panish.jpg' alt=''/>
-
-						<span class='about-middle-att-title'>Brian Panish</span><!-- about-middle-att-title -->
-
-						<div class='about-middle-att-content content'>
-						
-							<p>Brian Panish is a Los Angeles personal injury lawyer and one of the leading trial attorneys in the United States. He has obtained six (6) verdicts over $50 million in personal injury cases, more than 100 verdicts and settlements over $10 million, and 250 verdicts of over $1 million. Since 2011 he has won over $500 million for plaintiffs in personal injury, car accident, wrongful death, and business litigation.</p>
-
-							<div class='additional-content'>
-							
-								<h3>Mr. Panish’s recent awards include:</h3>
-
-								<ul>
-
-								<li>2019 Top 100 Lawyers in California by Daily Journal</li>
-
-								<li>2019 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-								<li>2019 Plaintiffs Lawyer Trailblazer by National Law Journal</li>
-
-								<li>2019 California Lawyer Attorney of the Year Award in the area of Personal Injury</li>
-
-								<li>2019 Best Lawyers® Lawyer of the Year in Personal Injury Litigation – Plaintiffs</li>
-
-								<li>2019 Titan of the Plaintiffs Bar by Law360</li>
-
-								<li>2018 Top 100 Lawyers in California by Daily Journal</li>
-
-								<li>2018 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-								<li>2018 Daniel O’Connell Award – by Irish American Bar Association-Los Angeles</li>
-
-								<li>2018 Lawyer of the Year in Aviation Law by Best Lawyers© in Los Angeles</li>
-
-								<li>2017 Top 100 Lawyers in California by the Daily Journal</li>
-
-								<li>2017 Top Plaintiff Lawyers in California by the Daily Journal</li>
-
-								<li>2017 Civil Advocate Award by the Association of Southern California Defense Council</li>
-
-								</ul>
-
-							<p>You can read more about Brian Panish on his main bio page <a href="">here.</a></p>
-							
-							</div><!-- additional-content -->
-						
-						</div><!-- about-middle-att-content -->
-
-						<a class='button-three about-read-more'>Read More</a><!-- button-two -->
-					
-					</div><!-- about-middle-att -->
-
-					<div class='about-middle-att'>
-					
-						<img src='<?php bloginfo('template_directory');?>/images/att-brian-panish.jpg' alt=''/>
-
-						<span class='about-middle-att-title'>Brian Panish</span><!-- about-middle-att-title -->
-
-						<div class='about-middle-att-content content'>
-						
-							<p>Brian Panish is a Los Angeles personal injury lawyer and one of the leading trial attorneys in the United States. He has obtained six (6) verdicts over $50 million in personal injury cases, more than 100 verdicts and settlements over $10 million, and 250 verdicts of over $1 million. Since 2011 he has won over $500 million for plaintiffs in personal injury, car accident, wrongful death, and business litigation.</p>
-
-							<div class='additional-content'>
-							
-								<h3>Mr. Panish’s recent awards include:</h3>
-
-								<ul>
-
-								<li>2019 Top 100 Lawyers in California by Daily Journal</li>
-
-								<li>2019 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-								<li>2019 Plaintiffs Lawyer Trailblazer by National Law Journal</li>
-
-								<li>2019 California Lawyer Attorney of the Year Award in the area of Personal Injury</li>
-
-								<li>2019 Best Lawyers® Lawyer of the Year in Personal Injury Litigation – Plaintiffs</li>
-
-								<li>2019 Titan of the Plaintiffs Bar by Law360</li>
-
-								<li>2018 Top 100 Lawyers in California by Daily Journal</li>
-
-								<li>2018 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-								<li>2018 Daniel O’Connell Award – by Irish American Bar Association-Los Angeles</li>
-
-								<li>2018 Lawyer of the Year in Aviation Law by Best Lawyers© in Los Angeles</li>
-
-								<li>2017 Top 100 Lawyers in California by the Daily Journal</li>
-
-								<li>2017 Top Plaintiff Lawyers in California by the Daily Journal</li>
-
-								<li>2017 Civil Advocate Award by the Association of Southern California Defense Council</li>
-
-								</ul>
-
-							<p>You can read more about Brian Panish on his main bio page <a href="">here.</a></p>
-							
-							</div><!-- additional-content -->
-						
-						</div><!-- about-middle-att-content -->
-
-						<a class='button-three about-read-more'>Read More</a><!-- button-two -->
-					
-					</div><!-- about-middle-att -->
-				
-				</div><!-- about-middle-col -->
+			</div><!-- about-middle-col -->
 
 				<div class='about-middle-col'>
 				
-				<div class='about-middle-att'>
+				<?php if ( have_rows( 'about_attorneys_two' ) ) : ?>
 					
-					<img src='<?php bloginfo('template_directory');?>/images/att-brian-panish.jpg' alt=''/>
-
-					<span class='about-middle-att-title'>Brian Panish</span><!-- about-middle-att-title -->
-
-					<div class='about-middle-att-content content'>
+					<?php while ( have_rows( 'about_attorneys_two' ) ) : the_row(); ?>
 					
-						<p>Brian Panish is a Los Angeles personal injury lawyer and one of the leading trial attorneys in the United States. He has obtained six (6) verdicts over $50 million in personal injury cases, more than 100 verdicts and settlements over $10 million, and 250 verdicts of over $1 million. Since 2011 he has won over $500 million for plaintiffs in personal injury, car accident, wrongful death, and business litigation.</p>
-
-						<div class='additional-content'>
+						<?php $post_object = get_sub_field( 'about_attorney' ); ?>
 						
-							<h3>Mr. Panish’s recent awards include:</h3>
-
-							<ul>
-
-							<li>2019 Top 100 Lawyers in California by Daily Journal</li>
-
-							<li>2019 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-							<li>2019 Plaintiffs Lawyer Trailblazer by National Law Journal</li>
-
-							<li>2019 California Lawyer Attorney of the Year Award in the area of Personal Injury</li>
-
-							<li>2019 Best Lawyers® Lawyer of the Year in Personal Injury Litigation – Plaintiffs</li>
-
-							<li>2019 Titan of the Plaintiffs Bar by Law360</li>
-
-							<li>2018 Top 100 Lawyers in California by Daily Journal</li>
-
-							<li>2018 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-							<li>2018 Daniel O’Connell Award – by Irish American Bar Association-Los Angeles</li>
-
-							<li>2018 Lawyer of the Year in Aviation Law by Best Lawyers© in Los Angeles</li>
-
-							<li>2017 Top 100 Lawyers in California by the Daily Journal</li>
-
-							<li>2017 Top Plaintiff Lawyers in California by the Daily Journal</li>
-
-							<li>2017 Civil Advocate Award by the Association of Southern California Defense Council</li>
-
-							</ul>
-
-						<p>You can read more about Brian Panish on his main bio page <a href="">here.</a></p>
+						<?php if ( $post_object ): ?>
 						
-						</div><!-- additional-content -->
-					
-					</div><!-- about-middle-att-content -->
+							<?php $post = $post_object; ?>
+							<?php setup_postdata( $post ); ?> 
 
-					<a class='button-three about-read-more'>Read More</a><!-- button-two -->
+							<div class='about-middle-att'>
+					
+								<?php $attorney_profile = get_field( 'attorney_profile' ); ?>
+								
+								<?php if ( $attorney_profile ) { ?>
+								
+									<img src="<?php echo $attorney_profile['url']; ?>" alt="<?php echo $attorney_profile['alt']; ?>" />
+								
+								<?php } ?>
+
+								<span class='about-middle-att-title'><?php the_title();?></span><!-- about-middle-att-title -->
+
+								<div class='about-middle-att-content content'>
+					
+									<?php the_sub_field( 'excerpt' ); ?>
+
+								<div class='additional-content'>
+						
+								<?php the_sub_field( 'content' ); ?>
+						
+							</div><!-- additional-content -->
+					
+						</div><!-- about-middle-att-content -->
+
+						<a class='button-three about-read-more'>Read More</a><!-- button-two -->
 				
-				</div><!-- about-middle-att -->
-
-				<div class='about-middle-att'>
-				
-					<img src='<?php bloginfo('template_directory');?>/images/att-brian-panish.jpg' alt=''/>
-
-					<span class='about-middle-att-title'>Brian Panish</span><!-- about-middle-att-title -->
-
-					<div class='about-middle-att-content content'>
-					
-						<p>Brian Panish is a Los Angeles personal injury lawyer and one of the leading trial attorneys in the United States. He has obtained six (6) verdicts over $50 million in personal injury cases, more than 100 verdicts and settlements over $10 million, and 250 verdicts of over $1 million. Since 2011 he has won over $500 million for plaintiffs in personal injury, car accident, wrongful death, and business litigation.</p>
-
-						<div class='additional-content'>
+					</div><!-- about-middle-att -->
+							
+				<?php wp_reset_postdata(); ?>
 						
-							<h3>Mr. Panish’s recent awards include:</h3>
+			<?php endif; ?>
+		
+		<?php endwhile; ?>
 
-							<ul>
-
-							<li>2019 Top 100 Lawyers in California by Daily Journal</li>
-
-							<li>2019 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-							<li>2019 Plaintiffs Lawyer Trailblazer by National Law Journal</li>
-
-							<li>2019 California Lawyer Attorney of the Year Award in the area of Personal Injury</li>
-
-							<li>2019 Best Lawyers® Lawyer of the Year in Personal Injury Litigation – Plaintiffs</li>
-
-							<li>2019 Titan of the Plaintiffs Bar by Law360</li>
-
-							<li>2018 Top 100 Lawyers in California by Daily Journal</li>
-
-							<li>2018 Top Plaintiff Lawyers in California by Daily Journal</li>
-
-							<li>2018 Daniel O’Connell Award – by Irish American Bar Association-Los Angeles</li>
-
-							<li>2018 Lawyer of the Year in Aviation Law by Best Lawyers© in Los Angeles</li>
-
-							<li>2017 Top 100 Lawyers in California by the Daily Journal</li>
-
-							<li>2017 Top Plaintiff Lawyers in California by the Daily Journal</li>
-
-							<li>2017 Civil Advocate Award by the Association of Southern California Defense Council</li>
-
-							</ul>
-
-						<p>You can read more about Brian Panish on his main bio page <a href="">here.</a></p>
-						
-						</div><!-- additional-content -->
-					
-					</div><!-- about-middle-att-content -->
-
-					<a class='button-three about-read-more'>Read More</a><!-- button-two -->
-				
-				</div><!-- about-middle-att -->
+	<?php endif; ?>
 				
 				</div><!-- about-middle-col -->
 			
@@ -307,7 +181,7 @@ get_header(); ?>
 
 		<div id='about-bottom'>
 
-			<span class="page-title page-large-content-title page-about-title">Accomplishments - Verdicts and Settlements</span>
+			<span class="page-title page-large-content-title page-about-title"><?php the_field( 'bottom_title' ); ?></span>
 
 			<?php //if(get_field('practice_area_description')) { ?>
 
@@ -315,7 +189,7 @@ get_header(); ?>
 
 	<div id='page-descrip'>
 
-		<p>Firm attorneys together have secured more than $10 billion in verdicts and settlements for clients over the years. Below are just a few examples of the landmark cases won by the attorneys of Panish Shea & Boyle LLP:</p>
+		<?php the_field( 'bottom_description' ); ?>
 
 		<?php //the_field( 'practice_area_description' ); ?>
 
@@ -329,34 +203,7 @@ get_header(); ?>
 
 <div id='about-bottom-content' class="content">
 
-	<ul>
-	<li>A <strong>$4.9 billion</strong> auto product liability jury verdict against General Motors involving a defective fuel tank.</li>
-
-	<li>A <strong>$58 million</strong> jury verdict for a man burned when his construction vehicle caught on fire</li>
-
-	<li>A <strong>$55.4 million</strong> jury verdict in a tire tread separation case.</li>
-
-	<li>A <strong>$39 million</strong> settlement against a company that maliciously prosecuted a violation of trade secrets case against two men, resulting in the collapse of their business.</li>
-
-	<li>A <strong>36.5 million</strong> jury verdict for a woman who was catastrophically injured in a big-rig truck crash.</li>
-
-	<li>A <strong>$34.5 million</strong> jury verdict for a young man who suffered a lower leg amputation after being hit by a truck.</li>
-
-	<li>A <strong>$27.4 million</strong> jury verdict in the death of a 4-year-old girl and injuries to three pedestrians after a city of San Francisco truck ran a red light, jumped the curb, struck and injured the victims.</li>
-
-	<li>A <strong>$21 million</strong> jury verdict for an Iraq War veteran who suffered a brain injury when the car in which he was a passenger was rear-ended by a commercial vehicle.</li>
-
-	<li>A <strong>$19.8 million</strong> jury verdict for a man who suffered severe burn and traumatic brain injuries when his rental home exploded.</li>
-
-	<li>A <strong>$17.45 millio</strong> bench award for a man whose family was killed when a military jet crashed into his home.</li>
-
-	<li>A <strong>$17 million</strong> jury verdict for a woman whose visually impaired son was killed when he fell between railway cars because of the lack of protective barriers and was crushed when the train moved.</li>
-
-	<li>A <strong>13.55 million</strong> settlement from the makers of the Apache helicopter that crashed in Iraq and injured two soldiers.</li>
-
-<p>View of our verdicts & settlements by practice area <a href="">here.</a></p>
-	
-	</ul>
+	<?php the_field( 'bottom_content' ); ?>	
 
 </div><!-- about-bottom-content  class="content"-->
 
