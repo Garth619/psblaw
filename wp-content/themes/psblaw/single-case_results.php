435043f976758f9b34611253cee910c16c1463db
Garrett Cullen <Garth619@users.noreply.github.com>
2020-11-12 13:46:43 -0800

    push

--- wp-content/themes/psblaw/single-case_results.php
+++ wp-content/themes/psblaw/single-case_results.php
@@ -14,7 +14,7 @@
 
 			<div class='page-content-inner content'>
 
-			<div class='fetured-on-wrapper'>
+			<div class='featured-on-wrapper'>
 
 				<span class='featured-one'>Featured On:</span><!-- featured-one -->
 			
@@ -26,7 +26,7 @@
 				
 						<div class='video-thumb'>
 	
-							<div class='mywisita wistia_embed wistia_async_<?php the_field( 'attorney_wistia_id' ); ?> popover=true popoverContent=thumbnail'></div><!-- wistia -->
+							<div class='mywisita wistia_embed wistia_async_7y9zdxmy0q popover=true popoverContent=thumbnail'></div><!-- wistia -->
 	
 							<div class='video-overlay'>
 	
@@ -41,12 +41,92 @@
 				<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
 				
 				</div><!-- featured-on-video -->
+
+				<div class='featured-content content'>
+				
+					<div class='featured-row'>
+					
+						<span class='featured-row-title'>Landmark Case:</span><!-- featured-row-title -->
+
+						<p>Anderson vs. GM (General Motors) Anderson vs. GM (General Motors) Anderson vs. GM (General Motors)</p>
+						
+					</div><!-- featured-row -->
+
+					<div class='featured-row'>
+					
+						<span class='featured-row-title'>Landmark Case:</span><!-- featured-row-title -->
+
+						<p>Anderson vs. GM (General Motors) Anderson vs. GM (General Motors) Anderson vs. GM (General Motors)</p>
+						
+					</div><!-- featured-row -->
+
+					<div class='featured-row'>
+					
+						<span class='featured-row-title'>Landmark Case:</span><!-- featured-row-title -->
+
+						<p>Anderson vs. GM (General Motors) Anderson vs. GM (General Motors) Anderson vs. GM (General Motors)</p>
+						
+					</div><!-- featured-row -->
+
+					<div class='featured-row'>
+					
+						<span class='featured-row-title'>Landmark Case:</span><!-- featured-row-title -->
+
+						<p>Anderson vs. GM (General Motors) Anderson vs. GM (General Motors) Anderson vs. GM (General Motors)</p>
+						
+					</div><!-- featured-row -->
+				
+				</div><!-- featured-content -->
 			
 				</div><!-- featured-on-inner -->
 			
-			</div><!-- fetured-on-wrapper -->
+			</div><!-- featured-on-wrapper -->
 
+			<div class='case-synopsis-wrapper'>
+			
+				<h2>Case Synopsis:</h2>
+
+				<p>A jury ordered General Motors to pay $4.9 billion to the Anderson family for a defectively designed fuel system that caused their Chevy Malibu to burst into flames after it was rear ended on December 24, 1993. Brian Panish of Panish Shea & Boyle LLP represented the Andersons in their auto product liability suit which resulted in the largest personal injury verdict in history.</p>
+
+				<div class="blockquote">
+
+					<p>“The clients that we represent come to us because they’ve got some wrongs that need to be made right, and there’s no one else that’s going to do it but us.”</p>
+
+					<span>-Brian Panish</span>
+
+			</div>
+			
+			</div><!-- case-synopsis-wrapper -->
+
+			<div class='case-articles'>
+			
+				<h2>Anderson vs GM Case Articles:</h2>
+
+				<div class='case-article-row'>
 				
+					<img class='case-article-image' src='<?php bloginfo('template_directory');?>/images/results-national-law-logo.jpg' alt=''/>
+
+					<a class='case-article-link' href=''>The Biggest Jury Verdict of 1999</a><!-- case-article-link -->
+
+					<p>A typical verdict last year was way up, but nothing like this one.</p>
+
+					<a class='case-article-link' href=''>The Biggest Jury Verdict of 1999</a><!-- case-article-link -->
+
+					<p>A typical verdict last year was way up, but nothing like this one.</p>
+				
+				</div><!-- case-article-row -->
+
+				<div class='case-article-row'>
+
+					<img class='case-article-image' src='<?php bloginfo('template_directory');?>/images/results-national-law-logo.jpg' alt=''/>
+
+					<a class='case-article-link' href=''>The Biggest Jury Verdict of 1999</a><!-- case-article-link -->
+
+					<p>A typical verdict last year was way up, but nothing like this one.</p>
+
+				</div><!-- case-article-row -->
+			
+			</div><!-- case-articles -->
 			
 			</div><!-- page-content-inner -->
 			
